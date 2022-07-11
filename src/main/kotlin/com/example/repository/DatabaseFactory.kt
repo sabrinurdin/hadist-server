bba8852c924773c20014b97ad0b6a86674c534ab
sabrinurdin <noerdinsabri07@gmail.com>
2022-07-11 20:39:19 +0800

    initial commite

--- src/main/kotlin/com/example/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/example/repository/DatabaseFactory.kt
@@ -3,6 +3,7 @@ package com.example.repository
 import com.example.model.Hadist
 import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.coroutine.insertOne
 import org.litote.kmongo.eq
 import org.litote.kmongo.reactivestreams.KMongo
 import org.litote.kmongo.setValue
@@ -20,11 +21,12 @@ class DatabaseFactory {
         return users
     }
 
+
     suspend fun getAllUsers():List<Hadist> = userCollection.find().toList()
     suspend fun getHadistById(id_hadist:String):List<Hadist> = userCollection.find(Hadist::id_hadis eq id_hadist).toList()
+    //suspend fun addHadistById(hadist: Hadist):Boolean = userCollection.insertOne(Hadist::id_hadis eq hadist.id_hadis,hadist)
     suspend fun deleteHadistById(id_hadist: String):Boolean = userCollection.deleteOne(Hadist::id_hadis eq id_hadist).wasAcknowledged()
-    suspend fun updateHadistById(hadist: Hadist):Boolean = userCollection.updateOne(Hadist::id_hadis eq hadist.id_hadis,hadist
-    )
+    suspend fun updateHadistById(hadist: Hadist):Boolean = userCollection.updateOne(Hadist::id_hadis eq hadist.id_hadis,hadist)
 //        setValue(
 //            Hadist::riwayat, riwayat,
 //            Hadist::riwayat, riwayat,
